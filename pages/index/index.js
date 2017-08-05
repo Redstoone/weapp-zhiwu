ee73ea370652c0d43d1b4572a30ccb09ed7a804a
redstoone <cy_jing_2010@163.com>
2017-08-05 11:12:26 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,50 @@
+//index.js
+//获取应用实例
+const app = getApp();
+
+Page({
+  data: {
+    composite: [
+      {
+        img: "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+        name: "夏日显白清新白色恋人爆钻款",
+        xz: "258",
+        yl: "366"
+      },
+      {
+        img: "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+        name: "夏日显白清新白色恋人爆钻款夏日显白清新白色恋人爆钻款",
+        xz: "258",
+        yl: "366"
+      },
+      {
+        img: "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+        name: "夏日清凉！显白人鱼蓝亮片钻甲",
+        xz: "258",
+        yl: "366"
+      }
+    ],
+    imgUrls: [
+      "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+      "http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg",
+      "http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg"
+    ],
+  },
+  onLoad: function() {
+    console.log("onLoad");
+    var that = this;
+  },
+
+  // 查看更多
+  toMore: function() {
+    wx.navigateTo({
+      url: "../logs/logs"
+    });
+  },
+  //详情
+  toGoods: function() {
+    wx.navigateTo({
+      url: "../logs/logs"
+    });
+  }
+});
