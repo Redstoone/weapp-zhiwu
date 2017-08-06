f750c2a8401a6faec5ce6b8c3ed515ac1e29976e
redstoone <cy_jing_2010@163.com>
2017-08-06 14:13:17 +0800

    使用weui

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,24 +1,26 @@
-//index.js
-//获取应用实例
 const app = getApp();
 
 Page({
   data: {
-    composite: [
+    goodsList: [
       {
-        img: "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+        img:
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
         name: "夏日显白清新白色恋人爆钻款",
         xz: "258",
         yl: "366"
       },
       {
-        img: "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+        img:
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
         name: "夏日显白清新白色恋人爆钻款夏日显白清新白色恋人爆钻款",
+
         xz: "258",
         yl: "366"
       },
       {
-        img: "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+        img:
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
         name: "夏日清凉！显白人鱼蓝亮片钻甲",
         xz: "258",
         yl: "366"
@@ -28,7 +30,7 @@ Page({
       "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
       "http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg",
       "http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg"
-    ],
+    ]
   },
   onLoad: function() {
     console.log("onLoad");
